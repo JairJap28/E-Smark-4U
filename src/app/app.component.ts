8c0d9d67000af3d67e937578d102235bd1f4bf35
Jair Jimenez Munevar <jairmunevar@gmail.com>
2020-12-28 20:19:14 -0500

    First Deploy
    - Remove Kommunicate chatbot.
    - Add Firebase.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+type MyWindow = (typeof window) & {
+  kommunicate: any;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'shoppingCampaings';
+  ngOnInit(): void {
+  }
 }
--- src/app/core/chatbot/chatbot.component.ts
+++ src/app/core/chatbot/chatbot.component.ts
@@ -14,14 +14,5 @@ export class ChatbotComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    (function(d, m){
-      var kommunicateSettings =
-          {"appId":"943728b7d64546ef70a1af8c4bd48e59","popupWidget":true,"automaticChatOpenOnNavigation":true};
-      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-      (window as MyWindow).kommunicate = m; m._globals = kommunicateSettings;
-  })(document, (window as MyWindow).kommunicate || {});
   }
-
 }
